fa1e496e45e98058ca1bc3de2d0d0060517e49d3
Andrey <shock.work@mail.ru>
2021-04-16 12:24:55 +0300

    add tweet saga to root saga

--- src/store/saga.root.ts
+++ src/store/saga.root.ts
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import tweetsSaga from './ducks/tweets/saga';
 import tagsSaga from './ducks/tags/saga';
+import tweetDataSaga from './ducks/tweet/saga';
 
 export default function* rootSaga() {
   yield all([
     tweetsSaga(),
+    tweetDataSaga(),
     tagsSaga()
   ]);
 }
