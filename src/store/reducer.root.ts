f1b03ba0ebe0e2281ce745b3ffb54363a9c9674f
Andrey <shock.work@mail.ru>
2021-04-16 12:24:37 +0300

    integrate one tweet reducer to root

--- src/store/reducer.root.ts
+++ src/store/reducer.root.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { tweets } from './ducks/tweets/reducer';
 import { tags } from './ducks/tags/reducer';
+import { tweet } from './ducks/tweet/reducer';
 
 const reducerRoot = combineReducers({
   tweets,
+  tweet,
   tags
 });
 
