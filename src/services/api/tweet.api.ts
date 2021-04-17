3c18302d7813089089780e104e48a864ff287888
Andrey <shock.work@mail.ru>
2021-04-17 12:45:20 +0300

    new action in tweet api for getting tweet by id

--- src/services/api/tweet.api.ts
+++ src/services/api/tweet.api.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
-import { TweetsStateInterface } from '../../store/ducks/tweets/contracts/state';
+import { TweetsInterface, TweetsStateInterface } from '../../store/ducks/tweets/contracts/state';
 
 class TweetsApi {
   async fetchTweets(): Promise<TweetsStateInterface['items']> {
     const { data } = await axios.get('/tweets');
+    return data;
+  }
+
+  async fetchTweetData(id: string): Promise<TweetsInterface> {
+    const { data } = await axios.get(`/tweets?_id=${id}`);
     return data;
   }
 }
