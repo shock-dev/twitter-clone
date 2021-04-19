7499074d891be33e3f9df29623e7630f2aa7af2b
Andrey <shock.work@mail.ru>
2021-04-19 09:04:31 +0300

    configure interceptor for axios

---
+++ src/core/axios.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  return config;
+});
+
+export default axios;
