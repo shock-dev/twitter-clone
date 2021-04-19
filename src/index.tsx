b159db2da684f90085b338719a1c343f9bc82048
Andrey <shock.work@mail.ru>
2021-04-19 22:10:31 +0300

    delete excess .css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import { ThemeProvider } from '@material-ui/core';
 import { Provider } from 'react-redux';
