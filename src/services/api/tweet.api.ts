a65ec11034dd328e15a4854d53fb77a66081aec4
Andrey <shock.work@mail.ru>
2021-04-19 08:11:05 +0300

    generic type for tweets api

--- src/services/api/tweet.api.ts
+++ src/services/api/tweet.api.ts
@@ -1,20 +1,25 @@
 import axios from 'axios';
 import { TweetsInterface, TweetsStateInterface } from '../../store/ducks/tweets/contracts/state';
 
+interface Response<T> {
+  status: string
+  data: T
+}
+
 class TweetsApi {
   async fetchTweets(): Promise<TweetsStateInterface['items']> {
-    const { data } = await axios.get('/tweets');
-    return data;
+    const { data } = await axios.get<Response<TweetsStateInterface['items']>>('/tweets');
+    return data.data;
   }
 
   async fetchTweetData(id: string): Promise<TweetsInterface> {
-    const { data } = await axios.get(`/tweets/${id}`);
-    return data;
+    const { data } = await axios.get<Response<TweetsInterface>>(`/tweets/${id}`);
+    return data.data;
   }
 
   async addTweet(tweet: TweetsInterface): Promise<TweetsInterface> {
-    const { data } = await axios.post('/tweets', tweet);
-    return data;
+    const { data } = await axios.post<Response<TweetsInterface>>('/tweets', tweet);
+    return data.data;
   }
 }
 
