9ba84f2ff639ff6358a92df0a4b63cdb7cebf65c
Andrey <shock.work@mail.ru>
2021-04-19 22:07:02 +0300

    started auto login realized

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
 import SignIn from './pages/SignIn';
 import UserPage from './pages/User';
+import { useDispatch } from 'react-redux';
+import AuthApi from './services/api/auth.api';
+import { setUser } from './store/ducks/user/actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const checkAuth = async () => {
+    try {
+      const user = await AuthApi.getMe();
+      dispatch(setUser(user));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <div className="App">
       <Switch>
