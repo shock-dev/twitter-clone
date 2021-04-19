ebb04ded3279cc51a92e9c3b81c8599596ae2995
Andrey <shock.work@mail.ru>
2021-04-19 16:09:07 +0300

    realize logic of sing in [without api]

--- src/pages/SignIn/components/LoginModal.tsx
+++ src/pages/SignIn/components/LoginModal.tsx
@@ -5,6 +5,20 @@ import { Button, TextField } from '@material-ui/core';
 import { useStylesSignIn } from '../index';
 import ModalBlock from '../../../components/ModalBlock';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+interface IFormInputs {
+  email: string
+  password: string
+}
+
+const LoginFormSchema = yup.object().shape({
+  email: yup.string().email('Неверная почта').required('Введите email'),
+  password: yup.string().min(6, 'Минимальная длина пароля 6 символов').required('Введите пароль')
+});
+
 interface LoginModalProps {
   open: boolean
   handleCloseModal: () => void
@@ -16,6 +30,14 @@ const LoginModal: React.FC<LoginModalProps> = ({
   handleCloseModal,
   classes
 }: LoginModalProps): React.ReactElement => {
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
+    resolver: yupResolver(LoginFormSchema)
+  });
+
+  console.log(errors);
+
+  const onSubmit = (data: IFormInputs) => console.log(data);
+
   return (
     <ModalBlock
       visible={open}
@@ -23,44 +45,53 @@ const LoginModal: React.FC<LoginModalProps> = ({
       classes={classes}
       title="Войти в аккаунт"
     >
-      <FormControl
-        className={classes.loginFormControl}
-        component="fieldset"
-        fullWidth
-      >
-        <FormGroup aria-label="position" row>
-          <TextField
-            className={classes.loginSideField}
-            autoFocus
-            id="email"
-            label="E-Mail"
-            InputLabelProps={{
-              shrink: true
-            }}
-            variant="filled"
-            type="email"
-            fullWidth
-          />
-          <TextField
-            className={classes.loginSideField}
-            id="password"
-            label="Пароль"
-            InputLabelProps={{
-              shrink: true
-            }}
-            variant="filled"
-            type="password"
-            fullWidth
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-          >
-            Войти
-          </Button>
-        </FormGroup>
-      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          className={classes.loginFormControl}
+          component="fieldset"
+          fullWidth
+        >
+          <FormGroup aria-label="position" row>
+            <TextField
+              className={classes.loginSideField}
+              autoFocus
+              id="email"
+              label="E-Mail"
+              InputLabelProps={{
+                shrink: true
+              }}
+              variant="filled"
+              type="email"
+              fullWidth
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+            <TextField
+              className={classes.loginSideField}
+              id="password"
+              label="Пароль"
+              InputLabelProps={{
+                shrink: true
+              }}
+              variant="filled"
+              type="password"
+              fullWidth
+              {...register('password')}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              fullWidth
+            >
+              Войти
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
     </ModalBlock>
   );
 };
