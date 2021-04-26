b6a7443516f2dcd65693ed54d7e89c8da1b425ef
Andrey <shock.work@mail.ru>
2021-04-26 19:21:28 +0300

    action for register

--- src/store/ducks/user/actions.ts
+++ src/store/ducks/user/actions.ts
@@ -2,11 +2,13 @@ import { Action } from 'redux';
 import { UserState } from './contracts/state';
 import { LoadingState } from '../../types';
 import { LoginFormProps } from '../../../pages/SignIn/components/LoginModal';
+import { RegisterFormProps } from '../../../pages/SignIn/components/RegisterModal';
 
 export enum UserActionType {
   SET_USER = 'user/SET_USER',
   SET_LOADING_STATE = 'user/SET_LOADING_STATE',
-  FETCH_LOGIN = 'user/FETCH_LOGIN'
+  FETCH_LOGIN = 'user/FETCH_LOGIN',
+  FETCH_REGISTER = 'user/FETCH_REGISTER'
 }
 
 export interface SetUserActionInterface extends Action<UserActionType> {
@@ -24,6 +26,11 @@ export interface FetchLoginActionInterface extends Action<UserActionType> {
   payload: LoginFormProps
 }
 
+export interface FetchRegisterActionInterface extends Action<UserActionType> {
+  type: UserActionType.FETCH_REGISTER
+  payload: RegisterFormProps
+}
+
 export const setUser = (payload: UserState['data']): SetUserActionInterface => ({
   type: UserActionType.SET_USER,
   payload
@@ -39,7 +46,13 @@ export const fetchLogin = (payload: LoginFormProps): FetchLoginActionInterface =
   payload
 });
 
+export const fetchRegister = (payload: RegisterFormProps): FetchRegisterActionInterface => ({
+  type: UserActionType.FETCH_REGISTER,
+  payload
+});
+
 export type UserAction =
   | SetUserActionInterface
   | SetUserLoadingStateActionInterface
   | FetchLoginActionInterface
+  | FetchRegisterActionInterface
