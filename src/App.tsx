d0ffb44d5ada11d11c9ae02c27b8764664c0fb4d
Andrey <shock.work@mail.ru>
2021-04-26 21:33:00 +0300

    realize logic of auth in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,40 @@ import Home from './pages/Home';
 import Layout from './pages/Layout';
 import SignIn from './pages/SignIn';
 import UserPage from './pages/User';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import { useDispatch, useSelector } from 'react-redux';
-import AuthApi from './services/api/auth.api';
-import { setUser } from './store/ducks/user/actions';
-import { selectIsAuth } from './store/ducks/user/selectors';
+import { fetchUserData } from './store/ducks/user/actions';
+import { selectIsAuth, selectUserIsLoading, selectUserIsNever } from './store/ducks/user/selectors';
+import { useHomeStyles } from './pages/theme';
 
 function App() {
+  const classes = useHomeStyles();
   const dispatch = useDispatch();
   const history = useHistory();
   const isAuth = useSelector(selectIsAuth);
-
-  const checkAuth = async () => {
-    try {
-      const user = await AuthApi.getMe();
-      dispatch(setUser(user));
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const isLoading = useSelector(selectUserIsLoading);
+  const isNever = useSelector(selectUserIsNever);
+  const isReady = !isLoading && !isNever;
 
   useEffect(() => {
-    if (isAuth) {
+    if (!isAuth && isReady) {
+      history.push('/signin');
+    } else {
       history.push('/home');
     }
-  }, [isAuth]);
+  }, [isAuth, isReady]);
 
   useEffect(() => {
-    checkAuth();
+    dispatch(fetchUserData());
   }, []);
+
+  if (!isReady) {
+    return (
+      <div className={classes.centered}>
+        <TwitterIcon color="primary" style={{ width: 80, height: 80 }} />
+      </div>
+    );
+  }
 
   return (
     <div className="App">
